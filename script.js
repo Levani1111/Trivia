56a139fea81c1fd39df587793c48db28651de40d
Levani Papashvili <papashvililevani@gmail.com>
2020-02-12 11:42:49 -0500

    New Updated /js/  /css/

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
- 
-    
+let result = document.querySelector('.result')
+let correct =document.querySelectorAll('.correct')
+let start= document.querySelector('.start')
+let option = document.querySelectorAll('.option')
+let q = document.querySelectorAll('.q')
+let directions= document.querySelector('.directions')
+
+
+let restart = document.querySelector('.restart')
+//console.log(q)
+
+
+
